87786a293eafd6e7343753873ec4f91196af5e5c
piuwpiuw <kevin.idir@gmail.com>
2018-03-20 10:47:19 +0100

    premier commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projet 0 papier</title>
+</head>
+<body>
+<form methode="post" action="contact.blade.php">
+	<label>Email :</Label> 
+		<input type="text" name="email" required />
+	<br/>
+	<label>Mot de passe :</label>
+		<input type="password" name="password" required />
+	<button>Envoyez</button>
+
+</body>
+</html>
