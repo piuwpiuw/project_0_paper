41ec0b93c136d26b65afd181c0aeedf5e344b995
deborah-powers <deborah.powers89@gmail.com>
2018-04-09 16:41:20 +0200

    04-09, methode propre pour recuperer un jeune dans la bdd

--- app/Http/Controllers/JeuneController.php
+++ app/Http/Controllers/JeuneController.php
@@ -12,8 +12,12 @@ class JeuneController extends Controller
         return view ('commun.error')->with ('errorMessage', $errorMessage);
     }
     // la fiche d'acceuil
-    public function reception(){
-        return view ('jeune.reception');
+    public function reception ($idJeune){
+        // find retrouve un jeune par son id
+        $jeune = Jeune::find ($idJeune);
+        if (! $jeune)
+            return $this->error ("Le jeune n°$idJeune n'a pas ete trouve dans la bdd");
+        else return view ('jeune.reception')->with ('$idJeune', $idJeune);
     }
     public function receptionFill ($idJeune){
         // pour que le jeune remplisse sa fiche
@@ -21,15 +25,20 @@ class JeuneController extends Controller
     // apres le login
     // TODO extraire le nom du conseiller a partir de son id
     public function home ($idJeune){
-        $jeuneList = Jeune::where ('id', 'LIKE', $idJeune)->get();
-        $nbYoung = count ($jeuneList);
-        if ($nbYoung ==0)
+        // find retrouve un jeune par son id
+        $jeune = Jeune::find ($idJeune);
+        if (! $jeune)
             return $this->error ("Le jeune n°$idJeune n'a pas ete trouve dans la bdd");
-        else return view ('jeune.home')->with ('jeune', $jeuneList[0]);
+        else return view ('jeune.home')->with ('jeune', $jeune);
     }
     private function show ($jeune){
         if (! $jeune) return $this->error ("Le jeune n'a pas ete trouve");
-        // TODO verifier si le jeune a rempli sa fiche
+        /* TODO
+        verifier si le jeune a rempli sa fiche
+            oui: l'ammenner vers la page home
+            non: l'ammenner vers la page reception (fiche d'accueil)
+        dans quelle table est la fiche d'accueil ?
+        */
         else return redirect()->route ('jeuneHome', $jeune);
     }
     // le jeune se logue
@@ -37,15 +46,14 @@ class JeuneController extends Controller
         return view ('commun.login');
     }
     public function validLogin(Request $request){
-        $jeuneList = Jeune::where ('mail_jeune', 'LIKE', $request->mail)->get();
-        $nbYoung = count ($jeuneList);
-        if ($nbYoung ==0)
+        $jeune = Jeune::where ('mail_jeune', 'LIKE', $request->mail)->first();
+        if (! $jeune)
             return $this->error ("Aucun jeune n'est identifie avec ce mail");
         else{
-            $password = $jeuneList[0]->password_jeune;
+            $password = $jeune->password_jeune;
             if ($password != $request->password)
                 return $this->error ("Le mot de passe ne correspond pas au mail");
-            else return $this->show ($jeuneList[0]);
+            else return $this->show ($jeune);
         }
     }
     // creer un jeune
@@ -57,19 +65,16 @@ class JeuneController extends Controller
          id_conseiller sera l'id du conseiller qui creer le compte
         ne sachant qu'en faire, je le traite comme un champ ordinaire
         */
-        // verifier si l'email est deja utilise dans la bdd
-        $jeuneList = Jeune::where ('mail_jeune', 'LIKE', $request->mail_jeune)->get();
-        $nbYoung = count ($jeuneList);
-        if ($nbYoung >0){
+        $jeune = Jeune::where ('mail_jeune', 'LIKE', $request->mail)->first();
+        if ($jeune)
             return $this->error ("Un jeune utilise deja ce mail");
-        }
         else{
             $jeune = Jeune::create ([
                 'id_conseiller'     => $request->id_conseiller,
                 'mail_jeune'        => $request->mail_jeune,
                 'password_jeune'    => $request->password_jeune
             ]);
-            return $this->home ($jeune);
+            return $this->show ($jeune);
         }
     }
 }
